03655decfe608cec4ee1fc0d483f9d619910a047
Luis Herrera <l.herrera.garnica@gmail.com>
2020-08-20 17:19:59 -0400

    se implementa la soluciOn y se cambian los switch de mi teclado, ahoar los azules están como cafés

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  id:number = 0;
+  public tareas:Array<{id: number, descripcion: string}> = [];
+
+  public agregaTarea(event){
+    const formulario = event.target.parentElement;
+    const [descripcion] = formulario;
+    const datos = {
+      id: this.id + 1,
+      descripcion: descripcion.value
+    }
+    this.tareas.push(datos);
+    this.id ++;
+    formulario.reset()
+  }
+
+  public listarTodos():Array<{id: number, descripcion: string}> {
+    return this.tareas;
+  }
+
+  /**
+   * Pudo ser directamente pasado el id
+   * pero este es otro approach
+   */
+  public eliminaElemento(event) {
+    const cajaAlerta = event.target.parentNode.parentNode;
+    const id = cajaAlerta.id.replace("id_", "");
+    // se filtra para no incluir el objeto con ese id
+    // en sus atributos.
+    this.tareas = this.tareas.filter( x => x.id != id)
+
+  }
 }
+
